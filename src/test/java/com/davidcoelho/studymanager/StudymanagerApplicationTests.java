d8a5105701bb19eac9f58cd4e3901d1826456d0e
David Coelho <davidmcoelho18@gmail.com>
2026-07-20 06:55:20 -0300

    Initialize Spring Boot project

---
+++ src/test/java/com/davidcoelho/studymanager/StudymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcoelho.studymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
