42196cb954e9fbddc60678625723985922ea6e9e
David Coelho <davidmcoelho18@gmail.com>
2026-09-07 19:32:30 -0300

    refactor: scope task service operations to authenticated user

--- studymanager-backend/src/main/java/com/davidcoelho/studymanager/task/repository/TaskRepository.java
+++ studymanager-backend/src/main/java/com/davidcoelho/studymanager/task/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.davidcoelho.studymanager.task.repository;
 
+import com.davidcoelho.studymanager.account.entity.User;
 import com.davidcoelho.studymanager.task.entity.Task;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,17 +9,19 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
-    List<Task> findBySubjectIgnoreCase(String subject);
-
-    List<Task> findByNameIgnoreCase(String name);
-
-    List<Task> findByNameContainingIgnoreCaseOrSubjectContainingIgnoreCase(
-            String name,
-            String subject
-    );
-
     List<Task> findByUserId(Integer userId);
 
     Optional<Task> findByIdAndUserId(Integer taskId, Integer userId);
+
+    List<Task> findBySubjectIgnoreCaseAndUser(String subject, User user);
+
+    List<Task> findByNameIgnoreCaseAndUser(String name, User user);
+
+    List<Task> findByNameContainingIgnoreCaseAndUserOrSubjectContainingIgnoreCaseAndUser(
+            String name,
+            User nameUser,
+            String subject,
+            User subjectUser
+    );
 
 }
